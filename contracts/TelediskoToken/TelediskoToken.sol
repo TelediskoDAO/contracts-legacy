1f6d10a89b3f9b76c2d3e461c593e1a07040ff21
Nicola Miotto <miotto@posteo.de>
2022-02-17 10:16:27 +0100

    remove unneeded lines

--- contracts/TelediskoToken/TelediskoToken.sol
+++ contracts/TelediskoToken/TelediskoToken.sol
@@ -7,9 +7,6 @@ import "./TelediskoTokenSnapshot.sol";
 import { Roles } from "../extensions/Roles.sol";
 
 contract TelediskoToken is TelediskoTokenSnapshot, AccessControl {
-    bytes32 public MANAGER_ROLE = keccak256("MANAGER_ROLE");
-    bytes32 public RESOLUTION_ROLE = keccak256("RESOLUTION_ROLE");
-
     constructor(string memory name, string memory symbol)
         TelediskoTokenSnapshot(name, symbol)
     {}
