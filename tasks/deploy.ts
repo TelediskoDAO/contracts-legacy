b3f7548600008648e85ef61b684cb9b4c94a8e2b
Nicola Miotto <miotto@posteo.de>
2022-05-18 16:36:51 +0200

    use proxies in deploy script

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,6 +1,10 @@
 import { keccak256, toUtf8Bytes } from "ethers/lib/utils";
 import { task } from "hardhat/config";
 import {
+  Voting,
+  ResolutionManager,
+  ShareholderRegistry,
+  TelediskoToken,
   ResolutionManager__factory,
   ShareholderRegistry__factory,
   TelediskoToken__factory,
@@ -28,32 +32,44 @@ task("deploy", "Deploy DAO", async (_, hre) => {
   console.log("  Network:", hre.network.name);
 
   console.log("  Deploy Voting.sol");
-  const votingContract = await votingFactory.deploy();
+  const votingContract = (await hre.upgrades.deployProxy(votingFactory, {
+    initializer: "initialize",
+  })) as Voting;
   await votingContract.deployed();
   console.log("    Address:", votingContract.address);
 
   console.log("  Deploy ShareholderRegistry.sol");
-  const shareholderRegistryContract = await shareholderRegistryFactory.deploy(
-    "Teledisko Share",
-    "TS"
-  );
+  const shareholderRegistryContract = (await hre.upgrades.deployProxy(
+    shareholderRegistryFactory,
+    ["Teledisko Share", "TS"],
+    {
+      initializer: "initialize",
+    }
+  )) as ShareholderRegistry;
   await shareholderRegistryContract.deployed();
   console.log("    Address:", shareholderRegistryContract.address);
 
   console.log("  Deploy TelediskoToken.sol");
-  const telediskoTokenContract = await telediskoTokenFactory.deploy(
-    "Teledisko Token",
-    "TT"
-  );
+  const telediskoTokenContract = (await hre.upgrades.deployProxy(
+    telediskoTokenFactory,
+    ["Teledisko Token", "TT"],
+    { initializer: "initialize" }
+  )) as TelediskoToken;
   await telediskoTokenContract.deployed();
   console.log("    Address:", telediskoTokenContract.address);
 
   console.log("  Deploy ResolutionManager.sol");
-  const resolutionManagerContract = await resolutionManagerFactory.deploy(
-    shareholderRegistryContract.address,
-    telediskoTokenContract.address,
-    votingContract.address
-  );
+  const resolutionManagerContract = (await hre.upgrades.deployProxy(
+    resolutionManagerFactory,
+    [
+      shareholderRegistryContract.address,
+      telediskoTokenContract.address,
+      votingContract.address,
+    ],
+    {
+      initializer: "initialize",
+    }
+  )) as ResolutionManager;
   await resolutionManagerContract.deployed();
   console.log("    Address:", resolutionManagerContract.address);
 
