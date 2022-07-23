ef84b8439718c580a765badf181747cd9a7da6f6
vrde <agranzot@mailbox.org>
2022-07-23 10:35:29 +0200

    Add amount

--- tasks/status.ts
+++ tasks/status.ts
@@ -8,18 +8,21 @@ import {
 
 task("mint-share", "Mint a share to an address")
   .addPositionalParam("account", "The address")
-  .setAction(async ({ account }: { account: string }, hre) => {
-    const contract = await loadContract(
-      hre,
-      ShareholderRegistry__factory,
-      "ShareholderRegistry"
-    );
+  .addPositionalParam("amount", "Amount to mint")
+  .setAction(
+    async ({ account, amount }: { account: string; amount: string }, hre) => {
+      const contract = await loadContract(
+        hre,
+        ShareholderRegistry__factory,
+        "ShareholderRegistry"
+      );
 
-    const tx = await contract.mint(account, parseEther("1"));
-    console.log("  Submitted tx", tx.hash);
-    const receipt = await tx.wait();
-    console.log("  Transaction included in block", receipt.blockNumber);
-  });
+      const tx = await contract.mint(account, parseEther(amount));
+      console.log("  Submitted tx", tx.hash);
+      const receipt = await tx.wait();
+      console.log("  Transaction included in block", receipt.blockNumber);
+    }
+  );
 
 task("set", "Set the status of an address")
   .addParam("status", "shareholder, investor, contributor, managing_board")
