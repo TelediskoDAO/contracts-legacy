d9e19f817eded8796053f9384b1ed72394522b7f
vrde <agranzot@mailbox.org>
2023-02-02 18:58:46 +0100

    Small improvs, add docs
    Co-authored-by: Nicola Miotto <sirnicolaz@users.noreply.github.com>

--- contracts/RedemptionController.sol
+++ contracts/RedemptionController.sol
@@ -44,7 +44,7 @@ contract RedemptionController is Initializable, AccessControlUpgradeable {
     }
 
     // TODO: improve naming to indicate that this is more than just MINT
-    struct Mint {
+    struct MintBudget {
         uint256 timestamp;
         uint256 amount;
     }
@@ -52,14 +52,14 @@ contract RedemptionController is Initializable, AccessControlUpgradeable {
     mapping(address => Redeemable[]) internal _redeemables;
     mapping(address => uint256) internal _redeemablesFirst;
 
-    mapping(address => Mint[]) internal _mints;
-    mapping(address => uint256) internal _mintsFirst;
+    mapping(address => MintBudget[]) internal _mintBudgets;
+    mapping(address => uint256) internal _mintBudgetsStartIndex;
 
     function afterMint(
         address account,
         uint256 amount
     ) external onlyRole(TOKEN_MANAGER_ROLE) {
-        _mints[account].push(Mint(block.timestamp, amount));
+        _mintBudgets[account].push(MintBudget(block.timestamp, amount));
     }
 
     function _addRedeemable(
@@ -82,86 +82,111 @@ contract RedemptionController is Initializable, AccessControlUpgradeable {
         uint256 amount
     ) external onlyRole(TOKEN_MANAGER_ROLE) {
         // Find tokens minted ofer the last 3 months of activity, no earlier than 15 months
-        if (_mints[account].length > 0) {
-            uint256 lastActivity = _mints[account][_mints[account].length - 1]
-                .timestamp;
+        if (_mintBudgets[account].length == 0) {
+            return;
+        }
 
-            // User can redeem tokens minted within 3 months since last activity
-            uint256 threshold = lastActivity - 30 days * 3;
-            // User cannot redeem tokens that were minted earlier than 15 months ago
-            uint256 earliestTimestamp = block.timestamp - 30 days * 15;
+        uint256 lastActivity = _mintBudgets[account][
+            _mintBudgets[account].length - 1
+        ].timestamp;
 
-            if (threshold < earliestTimestamp) {
-                threshold = earliestTimestamp;
-            }
+        // User can redeem tokens minted within 3 months since last activity
+        uint256 thresholdActivity = lastActivity - 30 days * 3;
+        // User cannot redeem tokens that were minted earlier than 15 months ago
+        uint256 earliestTimestamp = block.timestamp - 30 days * 15;
 
-            uint256 redemptionStarts = block.timestamp + TIME_TO_REDEMPTION;
+        // If thresholdActivity falls behind the 15 months threshold, we apply a
+        // cutoff.
+        if (thresholdActivity < earliestTimestamp) {
+            thresholdActivity = earliestTimestamp;
+        }
 
-            Mint[] storage accountMints = _mints[account];
-            uint256 i;
-            for (
-                i = _mintsFirst[account];
-                i < accountMints.length && amount > 0;
-                i++
+        // Calculate when the next redemption starts, that is today plus the
+        // time a contributor has to wait to redeem the tokens
+        uint256 redemptionStarts = block.timestamp + TIME_TO_REDEMPTION;
+
+        // Load the mint budgets for that account
+        MintBudget[] storage mintBudgets = _mintBudgets[account];
+        uint256 i;
+        for (
+            // Optimization: use the start index to avoid iterating over the
+            // whole array
+            i = _mintBudgetsStartIndex[account];
+            // Iterate until we reach the end of the budgets and we still have
+            // an amount to consume
+            i < mintBudgets.length && amount > 0;
+            i++
+        ) {
+            MintBudget storage mintBudget = mintBudgets[i];
+            // If the mint is within the activity window, consume the mint and
+            // create a new Redeemable object
+            if (
+                mintBudget.timestamp >= thresholdActivity &&
+                mintBudget.amount > 0
             ) {
-                Mint storage accountMint = accountMints[i];
-                if (accountMint.timestamp >= threshold) {
-                    if (amount >= accountMint.amount) {
-                        amount -= accountMint.amount;
+                if (amount >= mintBudget.amount) {
+                    amount -= mintBudget.amount;
 
-                        _addRedeemable(
-                            account,
-                            accountMint.amount,
-                            accountMint.timestamp,
-                            redemptionStarts
-                        );
-                        accountMint.amount = 0;
-                    } else {
-                        accountMint.amount -= amount;
+                    _addRedeemable(
+                        account,
+                        mintBudget.amount,
+                        mintBudget.timestamp,
+                        redemptionStarts
+                    );
+                    mintBudget.amount = 0;
+                } else {
+                    mintBudget.amount -= amount;
 
-                        _addRedeemable(
-                            account,
-                            amount,
-                            accountMint.timestamp,
-                            redemptionStarts
-                        );
-                        amount = 0;
-                    }
+                    _addRedeemable(
+                        account,
+                        amount,
+                        mintBudget.timestamp,
+                        redemptionStarts
+                    );
+                    amount = 0;
                 }
             }
-            _mintsFirst[account] = --i;
+        }
 
-            // + expired redeemables within range
-            Redeemable[] storage accountRedeemables = _redeemables[account];
+        // Optimization: save the start index for later use
+        _mintBudgetsStartIndex[account] = i - 1;
 
-            for (i = 0; i < accountRedeemables.length; i++) {
-                Redeemable storage accountRedeemable = accountRedeemables[i];
-                if (
-                    accountRedeemable.mintTimestamp >= threshold &&
-                    block.timestamp >= accountRedeemable.end &&
-                    accountRedeemable.amount > 0
-                ) {
-                    if (amount >= accountRedeemable.amount) {
-                        amount -= accountRedeemable.amount;
-                        _addRedeemable(
-                            account,
-                            accountRedeemable.amount,
-                            accountRedeemable.mintTimestamp,
-                            redemptionStarts
-                        );
+        // We may still have some amount to consume, that's why we check if
+        // there are some expired redeemables whose original mint is still in
+        // the activity threshold.
+        Redeemable[] storage accountRedeemables = _redeemables[account];
 
-                        accountRedeemable.amount = 0;
-                    } else {
-                        accountRedeemable.amount -= amount;
-                        _addRedeemable(
-                            account,
-                            amount,
-                            accountRedeemable.mintTimestamp,
-                            redemptionStarts
-                        );
+        for (i = 0; i < accountRedeemables.length && amount > 0; i++) {
+            Redeemable storage accountRedeemable = accountRedeemables[i];
+            if (
+                // If the redeemable expired, and
+                block.timestamp >= accountRedeemable.end &&
+                // if it wasn't completely redeemed
+                accountRedeemable.amount > 0 &&
+                // and the original mint is still in the activity threshold
+                accountRedeemable.mintTimestamp >= thresholdActivity
+            ) {
+                // Consume the redeemable
+                if (amount >= accountRedeemable.amount) {
+                    amount -= accountRedeemable.amount;
+                    _addRedeemable(
+                        account,
+                        accountRedeemable.amount,
+                        accountRedeemable.mintTimestamp,
+                        redemptionStarts
+                    );
 
-                        amount = 0;
-                    }
+                    accountRedeemable.amount = 0;
+                } else {
+                    accountRedeemable.amount -= amount;
+                    _addRedeemable(
+                        account,
+                        amount,
+                        accountRedeemable.mintTimestamp,
+                        redemptionStarts
+                    );
+
+                    amount = 0;
                 }
             }
         }
@@ -171,20 +196,20 @@ contract RedemptionController is Initializable, AccessControlUpgradeable {
         address account,
         uint256 amount
     ) external onlyRole(TOKEN_MANAGER_ROLE) {
-        Redeemable[] storage accountRedeemables = _redeemables[account];
+        Redeemable[] storage redeemables = _redeemables[account];
 
-        for (uint256 i = 0; i < accountRedeemables.length && amount > 0; i++) {
-            Redeemable storage accountRedeemable = accountRedeemables[i];
+        for (uint256 i = 0; i < redeemables.length && amount > 0; i++) {
+            Redeemable storage redeemable = redeemables[i];
             if (
-                block.timestamp >= accountRedeemable.start &&
-                block.timestamp < accountRedeemable.end
+                block.timestamp >= redeemable.start &&
+                block.timestamp < redeemable.end
             ) {
-                if (amount < accountRedeemable.amount) {
-                    accountRedeemable.amount -= amount;
+                if (amount < redeemable.amount) {
+                    redeemable.amount -= amount;
                     amount = 0;
                 } else {
-                    amount -= accountRedeemable.amount;
-                    accountRedeemable.amount = 0;
+                    amount -= redeemable.amount;
+                    redeemable.amount = 0;
                 }
             }
         }
